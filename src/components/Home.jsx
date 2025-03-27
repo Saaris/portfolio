d2ca745aeea7f64031b256345aca7f14d2006153
Sara Serti <saaris1985@gmail.com>
2025-03-27 20:12:08 +0100

    styling header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     return (
     <div className="home">
         <h2> Mitt Portfolio </h2>
-        <img alt="Portfolio picture" src={portfolioImage} />
+        <img alt="Portfolio picture" src='/.info/image.jpg' />
     </div>
     )
 }

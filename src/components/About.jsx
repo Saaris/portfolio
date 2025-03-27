d9182c38239c59af60ece27361ed612b8d99e13f
Sara Serti <saaris1985@gmail.com>
2025-03-27 18:57:01 +0100

    About component

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import { Link} from react-router
+
+const About = () => (
+
+    
+
+        <div className='about-me'>
+            <h3>Vem är jag</h3>
+            <Link to="/portfolio"> Portfolio</Link>
+        </div>
+)
+export default About
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Home from '.components/Home.jsx'
+import { HashRouter } from 'react-router'
+import About from '/components/About.jsx'
+
+const router = HashRouter (
+  [
+    {
+    path: "/",
+    Component: App,
+    children: 
+    [
+      {
+      index: true,
+      Component: Home
+      },
+      {
+        path: '/about',
+        Component: About
+      }
+    ]
+  }
+  ]
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

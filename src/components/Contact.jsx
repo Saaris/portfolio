b3261bafaf99b0a07edb937a63a7d45b5c859ebc
Sara Serti <saaris1985@gmail.com>
2025-03-27 19:25:27 +0100

    degree and contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router'
+
+const Contact = () => (
+    <div className='contact'>
+        <h3>Kontaktuppgifter</h3>
+        <Link to="/Contact"></Link>   
+    </div>
+)
---
+++ src/components/Degree.jsx
@@ -0,0 +1,8 @@
+import { Links } from 'react-router'
+
+const Degree = () => (
+    <div className='degree'>
+        <h3>Utbildning</h3>
+        <Link to="/Degree"></Link>
+    </div>
+)
